3c87a998cd3006fe5f9f1cb6972682a4d4e64fe4
Edgar Lins <edgarlins.dev@gmail.com>
2024-06-24 12:10:56 -0300

    Class: Component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,15 @@
 import { Container } from './styles';
 
+import { Button } from '../../components/Button';
+
 export function Details() {
   
   return (
     <Container>
       <h1>Hello, world!</h1>
       <span>Edgar Lins</span>
+
+      <Button />
     </Container>
   )
 }
