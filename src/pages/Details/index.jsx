032bbb712ffb62227820b2fc4511d6d236f79f74
Edgar Lins <edgarlins.dev@gmail.com>
2024-06-24 10:49:28 -0300

    Class: CSS in JS

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,8 +1,11 @@
-import "./styles.css";
+import { Container } from './styles';
 
 export function Details() {
   
   return (
-    <h1>Hello, world!</h1>
+    <Container>
+      <h1>Hello, world!</h1>
+      <span>Edgar Lins</span>
+    </Container>
   )
 }
