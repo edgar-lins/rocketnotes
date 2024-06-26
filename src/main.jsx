25b4d6e4b2c91ea17e4f534e27dceeb3c72c6297
Edgar Lins <edgarlins.dev@gmail.com>
2024-06-26 10:43:02 -0300

    Class: Applying Grid at Home

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
-import { Details } from './pages/Details/';
+import { Home } from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
