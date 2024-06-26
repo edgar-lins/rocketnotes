45f49fc26eae9267313052283057e39b6ac19da4
Edgar Lins <edgarlins.dev@gmail.com>
2024-06-26 10:45:49 -0300

    Class: Logo

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -18,7 +18,20 @@ export const Container = styled.div`
 
 export const Brand = styled.div`
     grid-area: brand;
-    background-color: red;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    > h1 {
+        font-size: 24px;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
 `;
 
 export const Menu = styled.ul`
