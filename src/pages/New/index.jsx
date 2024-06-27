f84adcd3cca327cd0049ef2da1477b1fe6c44c18
Edgar Lins <edgarlins.dev@gmail.com>
2024-06-27 11:59:59 -0300

    Class: Header New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,23 @@
+import { Input } from '../../components/Input';
 import { Header } from '../../components/Header';
 
-import { Container } from './styles';
+import { Container, Form } from './styles';
 
 export function New() {
     return (
         <Container>
             <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">voltar</a>
+                    </header>
+
+                    <Input placeholder="Título" />
+                </Form>
+            </main>
         </Container>
     )
 }
